4c47870af5d2c3e75ee0cabba66c31bf8c6047f7
Joshua Davian <joshuadavian@gmail.com>
2022-11-20 20:25:06 +0700

    workout model & schema

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Workout = require('../models/Workout');
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -9,8 +10,24 @@ router.get('/:id', (req, res) => {
     res.json({ message: 'Get single workout.' });
 });
 
-router.post('/', (req, res) => {
-    res.json({ message: 'Create new workout.' });
+router.post('/', async (req, res) => {
+    const { title, reps, load } = req.body;
+
+    try {
+        const workout = await Workout.create({
+            title,
+            reps,
+            load,
+        });
+
+        res.status(201).json(workout);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+
+    // res.json({ message: 'Create new workout.' });
 });
 
 router.patch('/:id', (req, res) => {
