ea3fe15e1b6deedb3be765d21acb55111193373c
Joshua Davian <joshuadavian@gmail.com>
2022-11-20 20:07:10 +0700

    configure prettier

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'Get all workouts.' });
+    res.json({ message: 'Get all workouts.' });
 });
 
 router.get('/:id', (req, res) => {
-  res.json({ message: 'Get single workout.' });
+    res.json({ message: 'Get single workout.' });
 });
 
 router.post('/', (req, res) => {
-  res.json({ message: 'Create new workout.' });
+    res.json({ message: 'Create new workout.' });
 });
 
 router.patch('/:id', (req, res) => {
-  res.json({ message: 'Update single workout.' });
+    res.json({ message: 'Update single workout.' });
 });
 
 router.delete('/:id', (req, res) => {
-  res.json({ message: 'Delete single workout.' });
+    res.json({ message: 'Delete single workout.' });
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method);
-  next();
+    console.log(req.path, req.method);
+    next();
 });
 
 // Routes
@@ -20,14 +20,12 @@ app.use(`${URL}/workouts`, workoutRoutes);
 
 // Database
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(
-        `Connected to database & listening on port ${process.env.PORT}...`
-      );
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Connected to database & listening on port ${process.env.PORT}...`);
+        });
+    })
+    .catch((error) => {
+        console.log(`Database error : ${error}`);
     });
-  })
-  .catch((error) => {
-    console.log(`Database error : ${error}`);
-  });
