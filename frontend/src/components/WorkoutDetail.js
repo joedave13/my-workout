ca5a87a96ebac5f26b560eb9515548837bbdcce0
Joshua Davian <joshuadavian@gmail.com>
2022-11-22 14:42:49 +0700

    delete workout

--- frontend/src/components/WorkoutDetail.js
+++ frontend/src/components/WorkoutDetail.js
@@ -1,4 +1,19 @@
+import { useWorkoutContext } from '../hooks/useWorkoutContext';
+
 const WorkoutDetail = ({ workout }) => {
+    const { dispatch } = useWorkoutContext();
+
+    const handleClick = async () => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE',
+        });
+        const json = await response.json();
+
+        if (response.ok) {
+            dispatch({ type: 'DELETE_WORKOUT', payload: json });
+        }
+    };
+
     return (
         <div className='workout-details'>
             <h4>{workout.title}</h4>
@@ -9,6 +24,7 @@ const WorkoutDetail = ({ workout }) => {
                 <strong>Reps :</strong> {workout.reps}
             </p>
             <p>{workout.createdAt}</p>
+            <span onClick={handleClick}>Delete</span>
         </div>
     );
 };
