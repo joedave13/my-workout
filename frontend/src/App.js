a449439e1f55f17603f7093631ee91a04d84f26c
Joshua Davian <joshuadavian@gmail.com>
2022-11-25 14:22:12 +0700

    login & register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
     return (
@@ -10,6 +12,8 @@ function App() {
                 <div className='pages'>
                     <Routes>
                         <Route path='/' element={<Home />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route path='/register' element={<Register />} />
                     </Routes>
                 </div>
             </BrowserRouter>
